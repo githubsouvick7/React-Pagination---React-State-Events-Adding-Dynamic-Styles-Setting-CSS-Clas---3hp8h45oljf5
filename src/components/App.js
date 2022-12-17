7909a49055857f00972ec1b1b27efe43e0af1a5b
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2022-12-17 21:00:30 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,13 @@ import PostList from './PostList';
 import fetchPosts from '../api/fetchPosts';
 import PaginationButtonsList from './PaginationButtonsList';
 const App = () => {
-    const [posts, setPosts] = useState([]);
-    const [page, setPage] = useState(1);
-    const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
 
-    const handlePage = (pageNumber) => {
-      setPage(pageNumber);
-    }
+  const handlePage = (pageNumber) => {
+    setPage(pageNumber);
+  }
 
   useEffect(() => {
     const getData = async (page, limit) => {
@@ -22,12 +22,12 @@ const App = () => {
     }
     getData(page, 5);
   }, [page])
-  
+
   return (
     <div id="main">
-      
-      {loading ? <Loader /> : <PostList posts= {posts}/>}
-      <PaginationButtonsList page={page} handlePage={handlePage}/>
+
+      {loading ? <Loader /> : <PostList posts={posts} />}
+      <PaginationButtonsList page={page} handlePage={handlePage} />
     </div>
   )
 }
